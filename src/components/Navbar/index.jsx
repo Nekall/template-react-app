eb032fd7b6c9c50766aabe31426888ee7cb4cdb3
Neka <nekoanchor@yahoo.fr>
2021-05-10 11:37:51 +0200

    have a good time

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import reactLogo from 'assets/images/react.svg';
 
 const Navbar = () => (
